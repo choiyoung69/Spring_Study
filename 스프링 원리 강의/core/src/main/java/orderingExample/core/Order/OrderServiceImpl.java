da78e5603596869e73cbc538c3515b28e0c33674
seoyoung choi <young01060994@gmail.com>
2024-05-18 15:03:17 +0900

    [스프링 강의] 인프런 김영한 - 스프링 원리

--- 스프링 원리 강의/core/src/main/java/orderingExample/core/Order/OrderServiceImpl.java
+++ 스프링 원리 강의/core/src/main/java/orderingExample/core/Order/OrderServiceImpl.java
@@ -8,9 +8,15 @@ import orderingExample.core.member.MemberRepository;
 import orderingExample.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
     //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     private final DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
--- 스프링 원리 강의/core/src/test/java/orderingExample/core/Order/OrderServiceTest.java
+++ 스프링 원리 강의/core/src/test/java/orderingExample/core/Order/OrderServiceTest.java
@@ -1,14 +1,23 @@
 package orderingExample.core.Order;
 
+import orderingExample.core.AppConfig;
 import orderingExample.core.member.Grade;
 import orderingExample.core.member.Member;
 import orderingExample.core.member.MemberService;
 import orderingExample.core.member.MemberServiceImpl;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 public class OrderServiceTest {
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
 
     @Test
     void createOrder(){

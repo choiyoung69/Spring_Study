f43554c14a10c160eeddc96e015658b94a6f77ec
choiyoung69 <choiyoung609@gmail.com>
2025-01-16 02:57:31 +0900

    [스프링 MVC1 복습] 요청 정보 Mapping 이해

---
+++ 스프링 mvc1/springmvcReview/src/main/java/hello/springmvcReview/SpringmvcReviewApplication.java
@@ -0,0 +1,13 @@
+package hello.springmvcReview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcReviewApplication.class, args);
+	}
+
+}
---
+++ 스프링 mvc1/springmvcReview/src/test/java/hello/springmvcReview/SpringmvcReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springmvcReview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

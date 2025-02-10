39e9888177152e1b805ab4a2129a10f7a6d468d9
choiyoung69 <choiyoung609@gmail.com>
2025-02-10 23:36:25 +0900

    [스프링 mvc1] 섹션 7 내용 복습하기

---
+++ 스프링 mvc1/springmvcReview/src/main/java/hello/springmvcReview/basic/HelloData.java
@@ -0,0 +1,9 @@
+package hello.springmvcReview.basic;
+
+import lombok.Data;
+
+@Data
+public class HelloData {
+    private String username;
+    private int age;
+}
--- 스프링 mvc1/springmvcReview/src/main/java/hello/springmvcReview/basic/requestmapping/MappingClassController.java
+++ 스프링 mvc1/springmvcReview/src/main/java/hello/springmvcReview/basic/requestmapping/MappingClassController.java
@@ -7,27 +7,27 @@ import org.springframework.web.bind.annotation.*;
 public class MappingClassController {
 
     @GetMapping
-    public String users(){
+    public String users() {
         return "get users";
     }
 
     @PostMapping
-    public String addUser(){
+    public String addUser() {
         return "post user";
     }
 
     @GetMapping("/{userId}")
-    public String findUser(@PathVariable String userId){
+    public String findUser(@PathVariable String userId) {
         return "get userId + " + userId;
     }
 
     @PatchMapping("/{userId}")
-    public String updateUser(@PathVariable String userId){
+    public String updateUser(@PathVariable String userId) {
         return "update User + " + userId;
     }
 
     @DeleteMapping("/{userId}")
-    public String deleteUser(@PathVariable String userId){
+    public String deleteUser(@PathVariable String userId) {
         return "delete user + " + userId;
     }
 
